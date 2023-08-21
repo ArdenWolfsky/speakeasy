b2cf375ebedb0b8c74d82dc01ae13708dd419da6
hyperdefined <contact@hyper.lol>
2023-08-21 14:33:43 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-cd /var/www/arden-new/
+main_folder="/var/www/arden-new/"
+cd $main_folder
 git pull
 rm -r _site/
 rm -r tag/
 python3 scripts/generate_tags.py
 bundle exec jekyll build
+
+for file in "$main_folder"/_site/*.html; do
+    if [ -f "$file" ]; then
+        html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype "$file" -o "$file"
+    fi
+done
