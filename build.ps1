7cf8d27efdb2c863a85722e801069b75571bd9f9
hyperdefined <contact@hyper.lol>
2023-08-21 15:53:50 -0400

    fix html minifier

--- build.ps1
+++ build.ps1
@@ -11,9 +11,10 @@ python3 scripts/generate_tags.py
 # Compile the site
 bundle exec jekyll build
 
-$files = Get-ChildItem -Path "$main_folder\_site\*.html"
+$files = Get-ChildItem -Path "$main_folder\_site\*.html" -Recurse
 
 foreach ($file in $files) {
+    Write-Output $file.FullName
     if ($file.PSIsContainer -eq $false) {
         & html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype "$file" -o "$file"
     }
--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+recursive_minify() {
+    local folder="$1"
+
+    for file in "$folder"/*.html; do
+        if [ -f "$file" ]; then
+            echo $file
+            html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype "$file" -o "$file"
+        fi
+    done
+
+    for subfolder in "$folder"/*; do
+        if [ -d "$subfolder" ]; then
+            recursive_minify "$subfolder"
+        fi
+    done
+}
+
 # Set the root path
 main_folder="/var/www/arden-new/"
 cd $main_folder
@@ -11,9 +29,4 @@ rm -r tag/
 python3 scripts/generate_tags.py
 # Compile the site
 bundle exec jekyll build
-
-for file in "$main_folder"/_site/*.html; do
-    if [ -f "$file" ]; then
-        html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype "$file" -o "$file"
-    fi
-done
+recursive_minify "$main_folder/_site"
